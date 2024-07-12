9aadd9d87f10cf5a91255c46c3c273e81d51fa6c
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-12 10:06:38 -0300

    fix: bug update router 6 version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from "./components/pages/Home"
+import Sale from "./components/pages/Sale"
+import Container from './components/layouts/Container';
+import Footer from './components/layouts/Footer'
+import Navbar from './components/layouts/Navbar'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Container> 
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/sale" element={<Sale/>}/>
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
