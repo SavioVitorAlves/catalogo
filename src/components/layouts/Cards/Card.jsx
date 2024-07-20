fa4b45dc84858d2e6dea87050dc2aa81f49df393
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-20 11:17:40 -0300

    refactor: update css

--- src/components/layouts/Cards/Card.jsx
+++ src/components/layouts/Cards/Card.jsx
@@ -1,11 +1,11 @@
 import style from './Card.module.css'
 import {useNavigate} from 'react-router-dom'
-function Card({img, preco, tamanho1, tamanho2, tamanho3, tamanho4}){
+function Card({img, preco, tamanho}){
     const navigate = useNavigate();
 
     const handleClick = () => {
         // Informações a serem passadas para a próxima página
-        const data = { img: img, preco: preco, tamanho1: tamanho1, tamanho2: tamanho2, tamanho3: tamanho3, tamanho4: tamanho4};
+        const data = { img: img, preco: preco, tamanho: tamanho};
 
         // Redireciona para a página de Vendas
         navigate('/sale', { state: data });
@@ -13,17 +13,15 @@ function Card({img, preco, tamanho1, tamanho2, tamanho3, tamanho4}){
 
     return(
         <div className={style.card} onClick={handleClick}>
-            <div className={style.div_img}></div>
+            <div className={style.div_img}><img src={img} alt="Blusas Femininas Masculinas e Insfantil"/></div>
             
             <div className={style.price}>
                 <div>
                 <p className={style.valor}>R$</p>
-                <p className={style.valor}>34,99</p>
+                <p className={style.valor}>{preco}</p>
                 </div>
                 <div>
-                    <p className={style.tamanho}>P</p>
-                    <p className={style.tamanho}>M</p>
-                    <p className={style.tamanho}>G</p>
+                    <p className={style.tamanho}>{tamanho}</p>
                 </div>
             </div>
             
