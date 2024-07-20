dca5149d5d2ee6f4701f42b3d62cf635e4c2941f
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-20 11:16:33 -0300

    refactor: remove text

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -12,8 +12,7 @@ function Navbar (){
                 <a href="#eletro">Eletronicos</a>
             </nav>
             <div>
-                <button ><a id='whats' href="https://api.whatsapp.com/send?phone=5563991080174" target='_blank'>Whatsapp</a></button>
-                <a href="">Add</a>
+                <button ><a id='whats' href="https://api.whatsapp.com/send?phone=5563991080174" target='_blank' rel="noreferrer">Whatsapp</a></button>
             </div>
 
         </navbar>
