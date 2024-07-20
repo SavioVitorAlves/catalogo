9d38cf9d707b6b34926dee87cace4fabb09f31b2
SavioVitorAlves <saviovitor2@gmail.com>
2024-07-20 11:18:48 -0300

    feat: add imgs

--- src/components/layouts/home/Catalogo/Cartaz.jsx
+++ src/components/layouts/home/Catalogo/Cartaz.jsx
@@ -1,9 +1,15 @@
 import style from './Cartaz.module.css'
+import cartaz1 from '../../../../imgs/cartaz_1.png'
+import cartaz2 from '../../../../imgs/cartaz_2.png'
 function Cartaz() {
     return (
         <section id='promocao' className={style.cartaz}>
-            <div></div>
-            <div></div>
+            <div>
+                <img src={cartaz1} alt="Promoções" />
+            </div>
+            <div>
+                <img src={cartaz2} alt="Relogios e Eletronicos" />
+            </div>
         </section>
     )
 }
